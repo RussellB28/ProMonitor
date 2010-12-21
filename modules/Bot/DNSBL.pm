94197aa174356c425c66de8e3d62dd95b39fd649
RussellB28 <Russell Bradford>
2010-12-21 01:18:08 +0000

    The DNSBL Module now supports several options via the config. The bot can be set to either akill or kill the user if the IP is found in the list. Optionally the entire DNSBL module can now be disabled via the config

--- modules/Bot/DNSBL.pm
+++ modules/Bot/DNSBL.pm
@@ -1,4 +1,6 @@
-use strict;
+# Copyright (c) 2010 ProMonitor. All rights reserved.
+
+#use strict;
 use warnings;
 use Net::DNS;
 use Config::Scoped;
@@ -8,6 +10,12 @@ package pBot::DNSBL;
 
 
 sub Check {
+
+	if(&pBot::config('dnsbl', 'active') eq lc("no"))
+	{
+		return 0;
+	}
+
 	my ( $ip ) = @_;
 	my ($ipp1,$ipp2,$ipp3,$ipp4)=split /\./,$ip;
 	my $reverse = "$ipp4.$ipp3.$ipp2.$ipp1";
@@ -16,6 +24,7 @@ sub Check {
    	my $parser = Config::Scoped->new( file => "$dconf/configs/bl/dnsbl.conf", );
    	my $config = $parser->parse;
 	my $count = 0;
+	my $bls;
 
 	for (my $i = 0; $i < 20; $i++)
 	{
@@ -31,19 +40,44 @@ sub Check {
 					my ($ipr1,$ipr2,$ipr3,$ipr4)=split /\./,$rr->address;
 					if($ipr4 > 0 and $ipr4 < 30)
 					{
-						&pBot::Functions::MSG(&pBot::config('me', 'chan'),">> HOST: $ip has been found in $config->{list}->{$i} (Reason: $ipr4)");
+						&pBot::Functions::MSG(&pBot::config('me', 'lchan'),>> HOST $ip has been found in $config->{list}->{$i} (Reason: $ipr4)");
+						$bls = $config->{list}->{$i};
 						$count++;
 					}
        			}
 			}
 		}
 	}
-		&pBot::Functions::MSG(&pBot::config('me', 'chan'),">> HOST: $ip found in $count blacklists");
 
+	if($count > 0)
+	{
+		&pBot::Functions::MSG(&pBot::config('me', 'lchan'),>> HOST $ip found in $count blacklists [Action: ".uc(&pBot::config('dnsbl', 'action'))."]");
+		if(&pBot::config('dnsbl', 'action') eq lc("kline"))
+		{
+			&pBot::Functions::KLINE(&pBot::config('dnsbl', 'time'),"$ip","Your host is listed in the following blacklist: $bls");
+			return 1;
+		}
+		elsif(&pBot::config('dnsbl', 'action') eq lc("akill"))
+		{
+			&pBot::Functions::AKILL(&pBot::config('dnsbl', 'time'),"*@".$ip,"Your host is listed in the following blacklist: $bls");
+			return 1;
+		}
+	}
+	else
+	{
+		&pBot::Functions::MSG(&pBot::config('me', 'lchan'),>> HOST $ip found in $count blacklists");
+		return 1;
+	}
 }
 
 
 sub CheckL {
+
+	if(&pBot::config('dnsbl', 'active') eq lc("no"))
+	{
+		return 0;
+	}
+
 	my ( $ip ) = @_;
 	my $dconf = `pwd`;
 	chomp $dconf;
@@ -52,11 +86,22 @@ sub CheckL {
 
 		if($config->{badip}->{$ip})
 		{
-			&pBot::Functions::MSG(&pBot::config('me', 'chan'),">> HOST: $ip is listed in ProBL (Reason: $config->{badip}->{$ip})");
+			&pBot::Functions::MSG(&pBot::config('me', 'lchan'),>> HOST $ip is listed in ProBL (Reason: $config->{badip}->{$ip}) [ACTION: ".uc(&pBot::config('dnsbl', 'action'))."]");
+
+                        if(&pBot::config('dnsbl', 'action') eq lc("kline"))
+			{
+				&pBot::Functions::KLINE(&pBot::config('dnsbl', 'time'),"*@$ip","Your host is listed in ProBL: $config->{badip}->{$ip}");
+				return 1;
+			}
+			elsif(&pBot::config('dnsbl', 'action') eq lc("akill"))
+			{
+				&pBot::Functions::AKILL(&pBot::config('dnsbl', 'time'),"*@".$ip,"Your host is listed in ProBL: $config->{badip}->{$ip}");
+				return 1;
+			}
 		}
 		else
 		{
-			&pBot::Functions::MSG(&pBot::config('me', 'chan'),">> HOST: $ip was not found in ProBL (Local List)");
+			&pBot::Functions::MSG(&pBot::config('me', 'lchan'),>> HOST $ip was not found in ProBL (Local List)");
 		}
 
 }
